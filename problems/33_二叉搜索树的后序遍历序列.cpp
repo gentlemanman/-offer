85a5256beddafd1e3c68f7e132190dc6469896b2
zhaohan <540583339@qq.com>
2019-04-04 20:35:25 +0800

    Update 33_二叉搜索树的后序遍历序列.cpp

--- problems/33_二叉搜索树的后序遍历序列.cpp
+++ problems/33_二叉搜索树的后序遍历序列.cpp
@@ -6,26 +6,19 @@
 using namespace std;
 
 bool dfs(vector<int>& sequence, int begin, int end){
-    if(sequence.empty() || begin > end) return false;
-
-    int root = sequence[end];
-    int i = begin;
+    if(begin > end) return true;
+    int i = 0;
     for(; i < end; ++i){
-        if(sequence[i] > root) break;
+        if(sequence[i] > sequence[end]) break;
     }
     for(int j = i; j < end; ++j){
-        if(sequence[j] < root) return false;
+        if(sequence[j] < sequence[end]) return false;
     }
-    bool left = true;
-    if(i > begin)
-        left = dfs(sequence, begin, i - 1);
-    bool right = true;
-    if(i < end - 1)
-        right = dfs(sequence, i, end - 1);
-    return left && right;
+    return  dfs(sequence, begin, i - 1) && dfs(sequence, i, end - 1);
 }
 
 bool VerifySquenceOfBST(vector<int> sequence) {
+    if(sequence.empty()) return false;
     return dfs(sequence, 0, sequence.size() - 1);
 }
 
