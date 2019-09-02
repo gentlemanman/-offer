2f42b9a78476e111714c1fa8cd2f35c7ff2c4227
gentlemanman <540583339@qq.com>
2019-09-02 21:00:49 +0800

    2019/9/2

--- main.cpp
+++ main.cpp
@@ -5,39 +5,18 @@
 #include <unordered_map>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-    TreeNode(int x) :
-            val(x), left(NULL), right(NULL) {
-    }
-};
-
-void dfs(TreeNode* root, int target, vector<int>& tmp, vector<vector<int>>& ans){
-    if(!root->left && !root->right){
-        if(target == root->val){
-            tmp.push_back(root->val);
-            ans.push_back(tmp);
-            tmp.pop_back();
-        }
-        return;
-    }
-
-    tmp.push_back(root->val);
-    if(root->left) dfs(root->left, target-root->val, tmp, ans);
-    if(root->right) dfs(root->right, target-root->val, tmp, ans);
-    tmp.pop_back();
+void fun(int *a, int *b){
+    *a = *a - *b;
+    *b = *a + *b;
+    *a = *b - *a;
 }
-
-vector<vector<int>> FindPath(TreeNode* root, int target){
-    vector<int> tmp;
-    vector<vector<int>> ans;
-    dfs(root, target, tmp, ans);
-    return ans;
-}
-
 
 int main()
 {
+
+    string str;
+    while(cin>>str){
+        cout<<str;
+    }
+
 }
